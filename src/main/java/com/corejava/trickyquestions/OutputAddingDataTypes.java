a056ec775f47acb043dd50bc7882e2ed145546f7
Anup Shimoga <anups141988@gmail.com>
2021-04-08 13:03:42 +0530

    OutputUnaryOperator problem

---
+++ src/main/java/com/corejava/trickyquestions/OutputAddingDataTypes.java
@@ -0,0 +1,8 @@
+package com.corejava.trickyquestions;
+
+public class OutputAddingDataTypes {
+public static void main(String[] args) {
+	int i = (byte)+(char)-(int)+(long)-1;
+	System.out.println(i);
+}
+}
