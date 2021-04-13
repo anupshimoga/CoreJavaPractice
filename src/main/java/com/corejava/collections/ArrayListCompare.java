4f22345f206bcd3089ad4908bb2ef23a8f58ed96
Anup Shimoga <anups141988@gmail.com>
2021-04-13 19:52:50 +0530

    Added all files

---
+++ src/main/java/com/corejava/collections/ArrayListCompare.java
@@ -0,0 +1,33 @@
+package com.corejava.collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArrayListCompare {
+	public static void main(String[] args) {
+		ArrayList<String> nameList = new ArrayList<String>(Arrays.asList("Amnup","Absysy","Tom", "Ram"));
+		ArrayList<String> nameList1 = new ArrayList<String>(Arrays.asList("Amnup","Rom","Tom", "Ram"));
+		ArrayList<String> nameList2 = new ArrayList<String>(Arrays.asList("Amnup","Ram","Absysy","Tom" ));
+		
+		Collections.sort(nameList);
+		Collections.sort(nameList2);
+		System.out.println(nameList.equals(nameList2));
+		System.out.println(nameList.equals(nameList1));
+		
+		//find all additional elements
+		nameList1.removeAll(nameList);
+		System.out.println(nameList1);
+		
+		//find out missing elelemnts
+		nameList.removeAll(nameList1);
+		System.out.println(nameList);
+		
+		//find out common list
+		nameList2.retainAll(nameList);
+		System.out.println(nameList2);
+		
+		nameList1.retainAll(nameList);
+		System.out.println(nameList1);
+	}
+}
